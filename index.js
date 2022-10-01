c60307a78b419109cd800e2671ca940c49c8d42d
Anne <edmondson.anne@gmail.com>
2022-10-01 14:12:05 -0600

    commit tree a4c28de0a97b6f2dd3709a3d0914a629814bf635

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']) =>
+        { return drivers.slice(0,2); };
+
+const returnLastTwoDrivers = (drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']) =>
+        { return drivers.slice(2); };
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier (multiplier) {
+     return function (fareNum) {
+        return fareNum * multiplier;
+    };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier (3);
+
+function selectDifferentDrivers (drivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers();
+};
+
+
+
+/* selectDifferentDrivers() — This function takes two arguments, an array of the names of Scuber's drivers and either the returnFirstTwoDrivers() or returnLastTwoDrivers() function. Based on these two arguments, selectDifferentDrivers() will return either the first two drivers or the last two drivers.
+ */
